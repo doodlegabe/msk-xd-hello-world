9fe66aef628d567860f0dd9c01becbe1b94ce789
Gabriel Walsh <gabriel.walsh@moleskine.com>
2019-02-11 14:36:35 -0500

    hello world

--- main.js
+++ main.js
@@ -1,19 +1,35 @@
-const {Rectangle, Color} = require("scenegraph");   // [1]
+const {Rectangle, Color, Text} = require("scenegraph");
 
-function rectangleHandlerFunction(selection) {      // [2]
 
-  const newElement = new Rectangle();             // [3]
-  newElement.width = 100;
-  newElement.height = 50;
-  newElement.fill = new Color("Purple");
 
-  selection.insertionParent.addChild(newElement); // [4]
-  newElement.moveInParentCoordinates(100, 100);   // [5]
-
+function drawBook(selection) {
+  const moleskineBook = new Rectangle();
+  const band = new Rectangle();
+  const msg = new Text();
+  const black = new Color("#1D1717");
+  moleskineBook.width = 175;
+  moleskineBook.height = 269;
+  msg.text = "Hello Moleskine";
+  msg.fill = black;
+  msg.fontSize = 18;
+  band.width = 11;
+  band.height = 280;
+  moleskineBook.fill = black;
+  moleskineBook.cornerRadii = {topLeft:0, topRight:20, bottomRight:20, bottomLeft:0};
+  band.setAllCornerRadii(10);
+  band.fill = black;
+  band.stroke = new Color("White");
+  band.strokeWidth = 3;
+  selection.insertionParent.addChild(moleskineBook);
+  selection.insertionParent.addChild(band);
+  selection.insertionParent.addChild(msg);
+  moleskineBook.moveInParentCoordinates(0, 6);
+  band.moveInParentCoordinates(130, 0);
+  msg.moveInParentCoordinates(0, 300);
 }
 
-module.exports = { // [6]
+module.exports = {
   commands: {
-    createRectangle: rectangleHandlerFunction
+    helloMoleskine: drawBook
   }
 };
